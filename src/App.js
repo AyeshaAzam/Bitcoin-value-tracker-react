bd4b1f2484d24f8c8df75d2e26cd8ee48706b8dc
AyeshaAzam <ayesh76@msn.com>
2021-07-25 01:25:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+  LineSeries,
+} from "react-vis";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bitcoin app</h1>
+      <XYPlot width={300} height={300}>
+        <HorizontalGridLines />
+        <LineSeries
+          data={[
+            { x: 1, y: 10 },
+            { x: 2, y: 5 },
+            { x: 3, y: 15 },
+          ]}
+        />
+        <XAxis />
+        <YAxis />
+      </XYPlot>
     </div>
   );
 }
